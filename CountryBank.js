fe7f90f2e3ddad211d69d8f005cff363464373e2
pgorade9 <prashant.gorade@gmail.com>
2024-01-23 16:45:42 +0530

    changes

--- CountryBank.js
+++ CountryBank.js
@@ -1,7 +1,7 @@
 CountryBank = [
     {
         "question": "What is capital of India ?",
-        "option1": "Gujrat",
+        "option1": "Algiers",
         "option2": "Delhi",
         "option3": "New Delhi",
         "option4": "Washington D.C",
@@ -9,8 +9,8 @@ CountryBank = [
     },
     {
         "question": "What is capital of USA ?",
-        "option1": "Gujrat",
-        "option2": "Delhi",
+        "option1": "La Paz",
+        "option2": "Addis Ababa",
         "option3": "New Delhi",
         "option4": "Washington D.C",
         "answer": "option4"
@@ -18,65 +18,121 @@ CountryBank = [
     {
         "question": "What is capital of England ?",
         "option1": "London",
-        "option2": "Delhi",
+        "option2": "Suva",
         "option3": "New Delhi",
         "option4": "Washington D.C",
         "answer": "option1"
     },
     {
-        "question": "What is capital of China ?",
-        "option1": "London",
+        "question": "What is capital of Chille ?",
+        "option1": "Accra",
         "option2": "Beijing",
-        "option3": "New Delhi",
+        "option3": "Santiago",
         "option4": "Washington D.C",
-        "answer": "option2"
+        "answer": "option3"
     },
     {
         "question": "What is capital of Germany ?",
-        "option1": "London",
+        "option1": "GeorgeTown",
         "option2": "Delhi",
         "option3": "New Delhi",
         "option4": "Berlin",
         "answer": "option4"
     },
     {
-        "question": "What is capital of Pakistan ?",
-        "option1": "London",
-        "option2": "Karachi",
+        "question": "What is capital of Austrailia ?",
+        "option1": "Libreville",
+        "option2": "rome",
         "option3": "New Delhi",
-        "option4": "Islamabad",
+        "option4": "Canberra",
         "answer": "option4"
     },
     {
-        "question": "What is capital of Bangladesh ?",
-        "option1": "London",
+        "question": "What is capital of Cuba ?",
+        "option1": "Banjul",
         "option2": "Beijing",
         "option3": "New Delhi",
-        "option4": "Dhaka",
+        "option4": "Nicosia",
         "answer": "option4"
     },
     {
         "question": "What is capital of France ?",
         "option1": "Paris",
         "option2": "Beijing",
-        "option3": "London",
+        "option3": "Moroni",
         "option4": "Washington D.C",
         "answer": "option1"
     },
     {
         "question": "What is capital of Canada ?",
-        "option1": "London",
+        "option1": "Finland",
         "option2": "Beijing",
         "option3": "New Delhi",
         "option4": "Ottawa",
         "answer": "option4"
     },
     {
-        "question": "What is capital of Angola ?",
-        "option1": "azerbaijan",
-        "option2": "Louanda",
-        "option3": "New Delhi",
+        "question": "What is capital of Austria ?",
+        "option1": "Malabo",
+        "option2": "Vienna",
+        "option3": "Louanda",
         "option4": "Ottawa",
+        "answer": "option2"
+    },
+    {
+        "question": "What is capital of Armenia ?",
+        "option1": "Brasila",
+        "option2": "Yerevan",
+        "option3": "Louanda",
+        "option4": "Quito",
+        "answer": "option2"
+    },
+    {
+        "question": "What is capital of Bulgaria ?",
+        "option1": "Porto Novo",
+        "option2": "Vienna",
+        "option3": "Conakry",
+        "option4": "Sofia",
         "answer": "option4"
+    },
+    {
+        "question": "What is capital of East Timor ?",
+        "option1": "Porto Novo",
+        "option2": "Diji Bouti",
+        "option3": "Conakry",
+        "option4": "Dili",
+        "answer": "option4"
+    },
+    {
+        "question": "What is capital of Eritria ?",
+        "option1": "Tallin",
+        "option2": "Asmara",
+        "option3": "Helsinki",
+        "option4": "Sofia",
+        "answer": "option2"
+    },
+    {
+        "question": "What is capital of Gabon ?",
+        "option1": "Santo Damingo",
+        "option2": "Asmara",
+        "option3": "Cairo",
+        "option4": "Libreville",
+        "answer": "option4"
+    },
+    {
+        "question": "What is capital of Gambia ?",
+        "option1": "Banjul",
+        "option2": "Nicosia",
+        "option3": "Prague",
+        "option4": "Praia",
+        "answer": "option1"
+    },
+    {
+        "question": "What is capital of Denmark ?",
+        "option1": "San Jose",
+        "option2": "Bangui",
+        "option3": "Copenhagen",
+        "option4": "Sofia",
+        "answer": "option3"
     }
 ]
